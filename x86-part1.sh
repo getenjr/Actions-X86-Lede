387680715a118e0291313a9deb7e4c36e34965d1
杨黄林 <yhl452493373@gmail.com>
2024-06-17 18:55:12 +0800

    Update x86-part1.sh

--- x86-part1.sh
+++ x86-part1.sh
@@ -36,7 +36,7 @@ sed -i 's/\/cgi-bin\/luci\/admin\/services\/shadowsocksr/\/cgi-bin\/luci\/admin\
 
 # NPS内网穿透的客户端NPC
 # lede自带npc，这里就不需要再下载了
-#git clone https://github.com/yhl452493373/npc.git package/npc
+git clone https://github.com/yhl452493373/npc.git package/npc
 git clone https://github.com/yhl452493373/luci-app-npc.git package/luci-app-npc
 #修正npc翻译问题
 mv package/luci-app-npc/po/zh_Hans package/luci-app-npc/po/zh-cn
@@ -45,18 +45,18 @@ mv package/luci-app-npc/po/zh_Hans package/luci-app-npc/po/zh-cn
 git clone -b dev --depth 1 https://github.com/vernesong/OpenClash.git package/luci-app-openclash
 
 # frp
-rm -rf feeds/packages/net/frp
-git clone https://github.com/yhl452493373/openwrt-frp.git feeds/packages/net/frp
-FRP_URL=$( curl -sL https://api.github.com/repos/fatedier/frp/releases | grep -P 'download/v[\d.]+/frp_[\d.]+_linux_amd64.tar.gz' | awk -F '"' '{print $4}' | awk 'NR==1{print}' )
-FRP_VERSION=$( echo $FRP_URL | awk -F '/' '{print $8}' | awk '{gsub(/v/,"");print $1}' )
-FRP_HASH=$( curl -sL https://codeload.github.com/fatedier/frp/tar.gz/v$FRP_VERSION | sha256sum | awk -F ' ' '{print $1}' )
+#rm -rf feeds/packages/net/frp
+#git clone https://github.com/yhl452493373/openwrt-frp.git feeds/packages/net/frp
+#FRP_URL=$( curl -sL https://api.github.com/repos/fatedier/frp/releases | grep -P 'download/v[\d.]+/frp_[\d.]+_linux_amd64.tar.gz' | awk -F '"' '{print $4}' | awk 'NR==1{print}' )
+#FRP_VERSION=$( echo $FRP_URL | awk -F '/' '{print $8}' | awk '{gsub(/v/,"");print $1}' )
+#FRP_HASH=$( curl -sL https://codeload.github.com/fatedier/frp/tar.gz/v$FRP_VERSION | sha256sum | awk -F ' ' '{print $1}' )
 # 更新frp源码到最新版本
-sed -i -e 's/^PKG_VERSION.*/PKG_VERSION:='''$FRP_VERSION'''/' feeds/packages/net/frp/Makefile
-sed -i -e 's/^PKG_HASH.*/PKG_HASH:='''$FRP_HASH'''/' feeds/packages/net/frp/Makefile
+#sed -i -e 's/^PKG_VERSION.*/PKG_VERSION:='''$FRP_VERSION'''/' feeds/packages/net/frp/Makefile
+#sed -i -e 's/^PKG_HASH.*/PKG_HASH:='''$FRP_HASH'''/' feeds/packages/net/frp/Makefile
 
 # FRP穿透
-rm -rf feeds/luci/applications/luci-app-frpc
-git clone https://github.com/yhl452493373/luci-app-frpc.git feeds/luci/applications/luci-app-frpc
+#rm -rf feeds/luci/applications/luci-app-frpc
+#git clone https://github.com/yhl452493373/luci-app-frpc.git feeds/luci/applications/luci-app-frpc
 
 # 关机
 git clone https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
@@ -65,3 +65,6 @@ git clone https://github.com/esirplayground/luci-app-poweroff package/luci-app-p
 #git clone https://github.com/yhl452493373/openwrt-packages.git package/my-packages
 # 修正定时重启中文语言问题
 #mv package/my-packages/luci-app-autoreboot/po/zh-cn package/my-packages/luci-app-autoreboot/po/zh_Hans
+
+# 自己固件中需要的驱动
+git clone https://github.com/yhl452493373/openwrt-driver.git package/my-driver
